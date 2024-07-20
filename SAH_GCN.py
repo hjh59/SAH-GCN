4214eb578357fe51f1ab22bee1af6c669f159c45
HHJ <136887710+hjh59@users.noreply.github.com>
2024-07-21 00:08:33 +0800

    Model
    The model of SAH-GCN

--- SAH_GCN.py
+++ SAH_GCN.py
@@ -155,24 +155,15 @@ class SupConLoss(nn.Module):
         self.scale_by_temperature = scale_by_temperature
 
     def forward(self, features, labels=None, mask=None):
-        """
-        输入:
-            features: 输入样本的特征，尺寸为 [batch_size, hidden_dim].
-            labels: 每个样本的ground truth标签，尺寸是[batch_size].
-            mask: 用于对比学习的mask，尺寸为 [batch_size, batch_size], 如果样本i和j属于同一个label，那么mask_{i,j}=1
-        输出:
-            loss值
-        """
         if not torch.is_tensor(labels):
             labels = torch.tensor(labels)
         features = F.normalize(features, p=2, dim=1)
         batch_size = features.shape[0]
-        # 关于labels参数
-        if labels is not None and mask is not None:  # labels和mask不能同时定义值，因为如果有label，那么mask是需要根据Label得到的
+        if labels is not None and mask is not None:  
             raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None:  # 如果没有labels，也没有mask，就是无监督学习，mask是对角线为1的矩阵，表示(i,i)属于同一类
+        elif labels is None and mask is None: 
             mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:  # 如果给出了labels, mask根据label得到，两个样本i,j的label相等时，mask_{i,j}=1
+        elif labels is not None:  
             labels = labels.contiguous().view(-1, 1)
             if labels.shape[0] != batch_size:
                 raise ValueError('Num of labels does not match num of features')
@@ -195,44 +186,17 @@ class SupConLoss(nn.Module):
         # compute logits
         anchor_dot_contrast = torch.div(
             torch.matmul(features, features.T),
-            self.temperature)  # 计算两两样本间点乘相似度
+            self.temperature) 
         # for numerical stability
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
         logits = anchor_dot_contrast - logits_max.detach()
         exp_logits = torch.exp(logits)
-        '''
-        logits是anchor_dot_contrast减去每一行的最大值得到的最终相似度
-        示例: logits: torch.size([4,4])
-        logits:
-            tensor([[ 0.0000, -0.0471, -0.3352, -0.2156],
-                    [-1.2576,  0.0000, -0.3367, -0.0725],
-                    [-1.3500, -0.1409, -0.1420,  0.0000],
-                    [-1.4312, -0.0776, -0.2009,  0.0000]])       
-        '''
-        # 构建mask
+
         logits_mask = torch.ones_like(mask).to(device) - torch.eye(batch_size).to(device)
         positives_mask = mask * logits_mask
         negatives_mask = 1. - mask
-        '''
-        但是对于计算Loss而言，(i,i)位置表示样本本身的相似度，对Loss是没用的，所以要mask掉
-        # 第ind行第ind位置填充为0
-        得到logits_mask:
-            tensor([[0., 1., 1., 1.],
-                    [1., 0., 1., 1.],
-                    [1., 1., 0., 1.],
-                    [1., 1., 1., 0.]])
-        positives_mask:
-        tensor([[0., 0., 1., 1.],
-                [0., 0., 0., 0.],
-                [1., 0., 0., 1.],
-                [1., 0., 1., 0.]])
-        negatives_mask:
-        tensor([[0., 1., 0., 0.],
-                [1., 0., 1., 1.],
-                [0., 1., 0., 0.],
-                [0., 1., 0., 0.]])
-        '''
-        num_positives_per_row = torch.sum(positives_mask, axis=1)  # 除了自己之外，正样本的个数  [2 0 2 2]
+
+        num_positives_per_row = torch.sum(positives_mask, axis=1)  
         denominator = (torch.sum(exp_logits * negatives_mask, axis=1, keepdims=True) +
                        torch.sum(exp_logits * positives_mask, axis=1, keepdims=True))
 
@@ -242,12 +206,7 @@ class SupConLoss(nn.Module):
 
         log_probs = (torch.sum(log_probs * positives_mask, axis=1)[num_positives_per_row > 0] /
                      num_positives_per_row[num_positives_per_row > 0])
-        '''
-        计算正样本平均的log-likelihood
-        考虑到一个类别可能只有一个样本，就没有正样本了 比如我们labels的第二个类别 labels[1,2,1,1]
-        所以这里只计算正样本个数>0的    
-        '''
-        # loss
+
         loss = -log_probs
         if self.scale_by_temperature:
             loss *= self.temperature
@@ -256,7 +215,7 @@ class SupConLoss(nn.Module):
 
 
 class SAH_GCN(nn.Module):
-    def __init__(self, config, num_labels, gcn_layers=1, max_length=256, gcn_dropout=0.1):
+    def __init__(self, config, num_labels, gcn_layers, max_length, gcn_dropout):
         super(SAH_GCN, self).__init__()
         config = BertConfig()
         self.bert = BertModel(config)
@@ -275,7 +234,7 @@ class SAH_GCN(nn.Module):
         self.classifier_speaker = nn.Linear(config.hidden_size * self.max_length, config.hidden_size)
         self.classifier_not_single = nn.Linear(config.hidden_size * 3 * (self.gcn_layers), config.hidden_size)
 
-        self.CL = SupConLoss(temperature=0.5, scale_by_temperature=True)
+        self.CL = SupConLoss(temperature, scale_by_temperature=True)
 
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
         self.turnAttention = nn.MultiheadAttention(embed_dim=config.hidden_size,
@@ -292,9 +251,9 @@ class SAH_GCN(nn.Module):
 
         self.LSTM_layers = nn.ModuleList(
             [TurnLevelLSTM(hidden_size=config.hidden_size,
-                           num_layers=2,
-                           lstm_dropout=0.2,
-                           dropout_rate=0.4) for i in range(self.gcn_layers)])
+                           num_layers=,
+                           lstm_dropout=,
+                           dropout_rate=) for i in range(self.gcn_layers)])
 
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
